a7ca37efc24afa0a33dffdd98cce6ff7ab30521f
NguyenHuyenTrang070899 <trang12345.vn@gmail.com>
2018-10-03 20:39:31 +0700

    Dictionary_1

---
+++ Dictionary_1/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Moi ban nhap danh sach tu:");
+        System.out.print("So tu muon nhap la:");
+        DictionaryCommandLine dictionary_command_line = new DictionaryCommandLine();
+        dictionary_command_line.dictionaryBasic();
+    }
+}
