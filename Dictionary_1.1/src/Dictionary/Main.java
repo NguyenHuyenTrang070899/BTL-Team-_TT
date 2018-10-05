3338f42d9a2d98e017ff426149347d11481ddba1
NguyenHuyenTrang070899 <trang12345.vn@gmail.com>
2018-10-05 19:31:38 +0700

    Dictionary version2

---
+++ Dictionary_1.1/src/Dictionary/Main.java
@@ -0,0 +1,9 @@
+package Dictionary;
+
+public class Main
+{
+	public static void main(String[] args) {
+		DictionaryCommandLine dictionary_command_line = new DictionaryCommandLine();
+		dictionary_command_line.dictionaryBasic();
+	}
+}
