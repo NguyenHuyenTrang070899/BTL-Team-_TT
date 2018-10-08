d194f0ee5deca63396954ed9cdf1bfd7e768cf78
NguyenHuyenTrang070899 <trang12345.vn@gmail.com>
2018-10-08 21:14:55 +0700

    Updated Main

--- Dictionary_1.2/src/CommandLine/Main.java
+++ Dictionary_1.2/src/CommandLine/Main.java
@@ -1,27 +1,35 @@
 package CommandLine;
 
 import java.util.Scanner;
-public class Main{
-	public static void main(String[] args) {
+
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
 		DictionaryCommandLine dictionary_command_line = new DictionaryCommandLine();
-		dictionary_command_line.setTxtDictionaryManagement();
-		
 		Scanner scan = new Scanner(System.in);
+		System.out.println("1. Insert From Commandline");
+		System.out.println("2. Show All Words");
+		System.out.println("3. Dictionary Lookup");
+		System.out.println("4. Dictionary Searcher");
+		System.out.println("5. Export To File");
+		System.out.println("6. Delete One Word From Dictionary");
+		System.out.println("7. Exit");
 		while(true){
-			dictionary_command_line.listOfOptions();
-			int number = scan.nextInt();
+			int number = Integer.parseInt(scan.nextLine());
 			switch(number){
 				case 1 : { dictionary_command_line.setDictionaryManagement();
 							break;}
-				case 2 : { dictionary_command_line.showAllWords();
+				case 2 : { dictionary_command_line.Show();
 							break;}
-				case 3 : { dictionary_command_line.setDictionaryLookup();
+				case 3 : { dictionary_command_line.LookUp();
 							break;}
-				case 4 : { dictionary_command_line.dictionarySearcher();
+				case 4 : { dictionary_command_line.Searcher();
 							break;}
-				case 5 : { dictionary_command_line.exportDictionaryManagerment();
+				case 5 : { dictionary_command_line.Export();
 							break;}
-				case 6 : { dictionary_command_line.deleteWordInDictionary();
+				case 6 : { dictionary_command_line.Delete();
 							break;}
 				case 7 : { System.out.println("Goodbye!");
 							break;}
@@ -30,4 +38,4 @@ public class Main{
 			if(number==7) break;
 		}
 	}
-}
+	}
