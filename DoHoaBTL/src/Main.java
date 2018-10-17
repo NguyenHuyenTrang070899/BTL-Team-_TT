11cbb03e0d67d109414a439194047e4684c8057e
ThaoIE3 <trinhthithao1999@gmail.com>
2018-10-17 11:19:02 +0700

    ver1

--- DoHoaBTL/src/Main.java
+++ DoHoaBTL/src/Main.java
@@ -1,4 +1,5 @@
 
+//package firstswingexample;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.BorderFactory;
@@ -15,11 +16,11 @@ import  java.awt.Color;
 class Dictionary extends JFrame implements ActionListener{
     JTextField Input;
     JTextArea Output;
-    JButton b1,b2,b3,b4;
+    JButton b1,b2,b3;
     JLabel english,vietnamese;
     JPanel pn,pn1,pn2;
-    dictionnary Dic = new  dictionnary();
-    static boolean anhViet = true;
+    Search Dic = new  Search();
+  //static boolean anhViet = true;
     private final Border raisedBevel = BorderFactory.createRaisedBevelBorder();
     Dictionary(){
 
@@ -31,16 +32,13 @@ class Dictionary extends JFrame implements ActionListener{
         Output.setBounds(290,40,350,400);
         Output.setFocusable(false);
 
-        b1=new JButton("Dịch");
-        b1.setBackground(Color.gray);
+        b1 = new JButton("Dịch");
         b1.setBounds(200,40,70,30);
 
         b2 = new JButton("Thêm");
-        b2.setBackground(Color.gray);
         b2.setBounds(200,80,70,30);
 
         b3 = new JButton("Xóa");
-        b3.setBackground(Color.gray);
         b3.setBounds(200,120,70,30);
 
         english = new JLabel("Anh");
@@ -49,21 +47,14 @@ class Dictionary extends JFrame implements ActionListener{
         english.setBounds(30,10,50,30);
         vietnamese.setBounds(150,10,50,30);
 
-
-        b4 = new JButton(">");
-        b4.setBackground(new Color(205 , 98, 125));
-        b4.setBounds(70,15,50,20);
-        b4.setFont(new Font("Tahoma",Font.PLAIN,14));
-
         b1.addActionListener(this);
         b2.addActionListener(this);
         b3.addActionListener(this);
-        b4.addActionListener(this);
-
+ 
         pn = new JPanel();
         pn.setLayout(null);
         pn.setBackground(new Color(139, 87, 39));
-        pn.add(b1);pn.add(Input); pn.add(Output); pn.add(b2);pn.add(b3); pn.add(b4);
+        pn.add(b1);pn.add(Input); pn.add(Output); pn.add(b2);pn.add(b3);
         pn.add(english);pn.add(vietnamese);
         add(pn);
         setSize(700,500);
@@ -71,26 +62,10 @@ class Dictionary extends JFrame implements ActionListener{
     }
     public void actionPerformed(ActionEvent e){
         if(e.getSource()==b1){
-            if(anhViet)
-                Output.setText("<html>" + Dic.searchAnhViet(Input.getText()) + "</html>");
-            else
-                Output.setText("  * " + Dic.searchVietAnh(Input.getText()));
-        }
-        if(e.getSource()==b4){
-            if(anhViet){
-                english.setBounds(150,10,50,30);
-                vietnamese.setBounds(30,10,50,30);
-                anhViet = false;
+                Output.setText( Dic.search(Input.getText()) );          
             }
-            else {
-                english.setBounds(30,10,50,30);
-                vietnamese.setBounds(150,10,50,30);
-                anhViet = true;
-            }
-
-        }
     }
     public static void main(String[] args) {
-        new Dictionary();
+       new Dictionary();
     }
 }
